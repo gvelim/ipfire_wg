fd1e82a8ff7f7e9a1e6eeff9561c5da096cd4ccf
gvelim <3185416+gvelim@users.noreply.github.com>
2025-09-28 16:30:09 +0100

    Update customfwd.sh

--- customfwd.sh
+++ customfwd.sh
@@ -59,7 +59,7 @@ process_port_fwd_rules() {
 
 # Main program
 # ===============================================================
-RED_IP=$(ifconfig red0 | awk '/inet /{sub(/addr:/,"",$2); print $2}')
+RED_IP=$(ifconfig ppp0| awk '/inet /{sub(/addr:/,"",$2); print $2}')
 
 
 # Check for the correct number of arguments
